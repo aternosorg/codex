fcf853a4d383065a43382e1cffaa04e3a536873c
Matthias Neid <matthias@aternos.org>
2019-02-11 02:54:59 +0100

    added automatable solution interface

---
+++ src/Analysis/AutomatableSolutionInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Aternos\Codex\Analysis;
+
+/**
+ * Interface AutomatableSolutionInterface
+ *
+ * This interface should be used to indicate
+ * that a solution can be solved automatically
+ * e.g. deletion/creation/modification of files
+ *
+ * @package Aternos\Codex\Analysis
+ */
+interface AutomatableSolutionInterface extends SolutionInterface
+{
+
+}
