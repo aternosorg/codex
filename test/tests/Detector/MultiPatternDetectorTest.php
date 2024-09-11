9f0380d690866d3f26de8cb4c8c868f84db5cff1
Paul Vogel <paul@aternos.org>
2024-09-11 14:18:31 +0200

    Add Test for MultiPatternDetector

---
+++ test/tests/Detector/MultiPatternDetectorTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Aternos\Codex\Test\Tests\Detector;
+
+use Aternos\Codex\Detective\MultiPatternDetector;
+use Aternos\Codex\Log\File\StringLogFile;
+use PHPUnit\Framework\TestCase;
+
+class MultiPatternDetectorTest extends TestCase
+{
+    public function testDetectSinglePattern(): void
+    {
+        $this->assertTrue((new MultiPatternDetector())
+            ->setLogFile(new StringLogFile("You can detect this."))
+            ->addPattern('/detect/')
+            ->detect()
+        );
+    }
+
+    public function testDetectMultiplePatterns(): void
+    {
+        $this->assertTrue((new MultiPatternDetector())
+            ->setLogFile(new StringLogFile("You can detect this and this."))
+            ->addPattern('/detect/')
+            ->addPattern('/and this/')
+            ->detect()
+        );
+    }
+
+    public function testNotDetectMissingFromMultiplePatterns(): void
+    {
+        $this->assertFalse((new MultiPatternDetector())
+            ->setLogFile(new StringLogFile("You can detect this and this."))
+            ->addPattern('/detect/')
+            ->addPattern('/and this/')
+            ->addPattern('/but not this/')
+            ->detect()
+        );
+    }
+}
